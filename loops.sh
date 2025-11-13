01fd3341d06fac6dd538a45bfb7d09bada6c8b9d
ghost ninja <hilarycyberghost@gmail.com>
2025-11-13 03:14:10 +0000

    loops complete

--- loops.sh
+++ loops.sh
@@ -1,47 +1,44 @@
 #!/bin/bash
 
-
-#case 1 the for loop
-echo "============the for loop case =========="
-
+#case1 for loop
 names="hilary bob john mary christian"
 
 for n in $names; do
     echo "hello $n"
 done
 
+echo "   "
 
+#case 2  while loop
 
-#case 2 for the while loop case
-echo "============the while loop case ==================="
 x=1
 while [ $x -le 3 ]; do
     echo "$x"
     x=$((x + 1))
 done
 
-
+echo "  "
 
 #case 4 until loop
-echo "===============the until loop now===================" 
+
 y=1
 until [ $y -gt 5 ]; do
     echo " $y"
     y=$((y + 1))
 done
 
-
+echo "  "
 
 #case 5 break / continue 
-echo "================break/continue================================================"
-for v in 2 4 6 8 10; do
+
+for v in 2 4 5 6 8 10; do
     if [ $v -eq 5 ]; then
-        echo "6 skipped "
+        echo "5 skipped "
         continue
     fi
 
     if [ $v -gt 8 ]; then
-        echo "$v (stop)"
+        echo "$v"
         break
     fi
 
